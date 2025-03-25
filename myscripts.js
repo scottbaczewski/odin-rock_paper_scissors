bf756a3ceeea6a239d9b41a96cbfbb4b167baab1
scottbaczewski <130002237+scottbaczewski@users.noreply.github.com>
2025-03-25 15:58:34 -0400

    Wrote getComputerChoice() returns random choice

--- myscripts.js
+++ myscripts.js
@@ -1,1 +1,17 @@
-console.log("Hello Javascript!")
+// 2. Function that randomly returns “rock”, “paper” or “scissors”
+
+    // declare choices
+    const choices = ['rock', 'paper', 'scissors'];
+
+    // function to randomly return choice
+    function getComputerChoice() {
+        return choices[Math.floor(Math.random() * choices.length)];
+    }
+
+// 3. Function to get players choice
+
+// 4. Declare the players score variables
+
+// 5. Write logic to play single round
+
+// 6. Write logic to play the entire game
