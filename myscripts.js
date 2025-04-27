3caaa65575a69df8479fbd25f6029a9d63098517
scottbaczewski <130002237+scottbaczewski@users.noreply.github.com>
2025-04-27 08:51:44 -0400

    Add output elements, need rewrite playRound/Game

--- myscripts.js
+++ myscripts.js
@@ -3,6 +3,11 @@
     // declare choices
     const choices = ['rock', 'paper', 'scissors'];
 
+    const rock = document.querySelector('button.rock');
+    const paper = document.querySelector('button.paper');
+    const scissors = document.querySelector('button.scissors');
+    const results = document.querySelector('div.results');
+
     // function to randomly return choice
     function getComputerChoice() {
         return choices[Math.floor(Math.random() * choices.length)];
@@ -10,13 +15,7 @@
 
 // 3. Function to get players choice
 
-    function getPlayerChoice() {
-        return prompt('Pick 1: rock, paper, or scissors');
-    }
-
 // 4. Declare the players score variables
-
-
 
 // 5. Write logic to play single round
 
@@ -28,48 +27,46 @@ function playGame() {
     let playerScore = 0;
     let computerScore = 0;
     let round = 0;
+    let roundOutput = '';
+
+    function getPlayerChoice() {
+        rock.addEventListener("click", () => {
+            return 'rock'});
+        paper.addEventListener("click", () => {
+            return 'paper'});
+        scissors.addEventListener("click", () => {
+            return 'scissors'});
+    }
     
     function playRound(playerChoice, computerChoice) {
         if (playerChoice == computerChoice) {
-            console.log(`Tie! Both picked ${playerChoice}`);
+            roundOutput = `Tie! Both picked ${playerChoice}`;
         } else if ((playerChoice == 'rock' && computerChoice == 'scissors') ||
             (playerChoice == 'paper' && computerChoice == 'rock') ||
             (playerChoice == 'scissors' && computerChoice == 'paper')) {
                 playerScore++;
                 round++;
-                console.log(`You win! ${playerChoice} beats ${computerChoice}`);
+                roundOutput = `You win! ${playerChoice} beats ${computerChoice}`;
             } else {
                 computerScore++;
                 round++;
-                console.log(`You lose! ${computerChoice} beats ${playerChoice}`);
+                roundOutput = `You lose! ${computerChoice} beats ${playerChoice}`;
             }
+            let p = document.createElement('p');
+            p.textContent = roundOutput;
+            results.appendChild(p);
     }
     
 
-    // for (; round < 5;) {
-    //     let playerSelection = getPlayerChoice().toLowerCase();
-    //     let computerSelection = getComputerChoice();
-    //     playRound(playerSelection, computerSelection);
-    //     console.log(`Round: ${round}, PlayerScore: ${playerScore}, ComputerScore: ${computerScore}`)
-    // }  
-    // if (playerScore >= 3) {
-    //     console.log(`You won the game ${playerScore} to ${computerScore}!`);
-    // } else console.log(`The computer won the game ${computerScore} to ${playerScore}!`);
-
-
-const rock = document.querySelector('button.rock');
-const paper = document.querySelector('button.paper');
-const scissors = document.querySelector('button.scissors');
-
-rock.addEventListener("click", () => {
-    playRound('rock', getComputerChoice())
-});
-paper.addEventListener("click", () => {
-    playRound('paper', getComputerChoice())
-});
-scissors.addEventListener("click", () => {
-    playRound('scissors', getComputerChoice())
-});
+    for (; round < 5;) {
+        let playerSelection = getPlayerChoice();
+        let computerSelection = getComputerChoice();
+        playRound(playerSelection, computerSelection);
+        console.log(`Round: ${round}, PlayerScore: ${playerScore}, ComputerScore: ${computerScore}`)
+    }  
+    if (playerScore >= 3) {
+        console.log(`You won the game ${playerScore} to ${computerScore}!`);
+    } else console.log(`The computer won the game ${computerScore} to ${playerScore}!`);
 
 }
 playGame()
